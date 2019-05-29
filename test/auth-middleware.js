103b50e6c3b586545b836c94958f59575269dc19
stefian <v_ianta@yahoo.com>
2019-05-29 18:22:03 -0400

    grouped 2 tests in a titled description() function

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,25 +2,28 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-// Unit test for 1 function
-it('should throw an error if no authorization header is present', function () {
-    // define Unit test context/scenario
-    const req = {
-        get: function () {
-            return null;
-        }
-    };
-    // passing the call through mocha and chai
-    expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Not authenticated.');
-});
+describe('Auth middleware', function () {
+    // Unit test for 1 function
+    it('should throw an error if no authorization header is present', function () {
+        // define Unit test context/scenario
+        const req = {
+            get: function () {
+                return null;
+            }
+        };
+        // passing the call through mocha and chai
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Not authenticated.');
+    });
 
-// Another test
-it('should throw an error if the authorization header is only one string', function () {
-    // define Unit test context/scenario
-    const req = {
-        get: function () {
-            return 'xyz';
-        }
-    };
-    expect(authMiddleware.bind(this, req, {}, () => {} )).to.throw();
-});
+    // Another test
+    it('should throw an error if the authorization header is only one string', function () {
+        // define Unit test context/scenario
+        const req = {
+            get: function () {
+                return 'xyz';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
+    });
+})
+
