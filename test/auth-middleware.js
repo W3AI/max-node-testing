73c251b8026c90fbea79f24ab7245f5f4d2d1fef
stefian <v_ianta@yahoo.com>
2019-05-29 19:49:06 -0400

    added a 3rd test if token cannot be verified

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -25,5 +25,15 @@ describe('Auth middleware', function () {
         };
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
     });
+
+    it('should throw an error if the token cannot be verified', function() {
+        // define Unit test context/scenario
+        const req = {
+            get: function () {
+                return 'Bearer xyz';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
 })
 
