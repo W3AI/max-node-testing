65cca7c026bb9b2e9dc57c5058ddfb2a6d1b929d
stefian <v_ianta@yahoo.com>
2019-05-30 15:04:38 -0400

    finalized function it() with 2 expectations and call for done() for async testing

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -4,12 +4,26 @@ const sinon = require('sinon');
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', function() {
-    it('should throw an error with code 500 if accessing the database fails', function() {
+describe('Auth Controller - Login', function () {
+
+    // to add done in function of it() for async/promisses 
+    it('should throw an error with code 500 if accessing the database fails', function (done) {
         sinon.stub(User, 'findOne');
         User.findOne.throws();
 
-        expect(AuthController.login);
+        const req = {
+            body: {
+                email: 'test@test.com',
+                password: 'tester'
+            }
+        };
+
+        AuthController.login(req, {}, () => { }).then(result => {
+            expect(result).to.be.an('error');
+            expect(result).to.have.property('statusCode', 500);
+            // wait for the function to execute by calling done() from above / provided by mocha
+            done();
+        });
 
         User.findOne.restore();
     });
